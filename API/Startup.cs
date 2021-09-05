d47a608c3efbf56e612cf86a7d60ab9e475c89cc
tylerhartsock <thartsock@kensbeverage.com>
2021-09-05 07:59:25 -0500

    end of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,8 +29,14 @@ namespace API
             );
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
 
-
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,7 @@ namespace API
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
